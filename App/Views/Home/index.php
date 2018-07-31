19cbc868e9a2627ecfbd3765ade2a0fdb3971826
b3n10 <francisben.lleve@gmail.com>
2018-07-31 23:38:56 +0800

    Fix $user to $user_object

--- App/Views/Home/index.php
+++ App/Views/Home/index.php
@@ -1,7 +1,7 @@
 <?php require_once dirname(__DIR__) . "/header.php"; ?>
 	<h1>Welcome :)</h1>
-	<?php if ($user): ?>
-	<p>Hi <?php echo htmlspecialchars($user->name); ?>. <a href="/logout">Log out</a></p>
+	<?php if ($user_object): ?>
+	<p>Hi <?php echo htmlspecialchars($user_object->name); ?>. <a href="/logout">Log out</a></p>
 	<?php else: ?>
 		<a href="/signup/new">Sign up</a> or <a href="/login/">log in</a>
 	<?php endif ?>
