13ba5cf157b785bf3030185738952e7945c3d4c5
b3n10 <francisben.lleve@gmail.com>
2018-08-06 21:11:18 +0800

    Add logout to nav

--- App/Views/Home/index.php
+++ App/Views/Home/index.php
@@ -2,7 +2,7 @@
 	<h1>Welcome :)</h1>
 	<?php if ($user_object): ?>
 	<p>
-	Hi <?php echo htmlspecialchars($user_object->name); ?>. <a href="/logout">Log out</a>
+	Hi <?php echo htmlspecialchars($user_object->name); ?>.
 	</p>
 	<?php else: ?>
 		<a href="/signup/new">Sign up</a> or <a href="/login/">log in</a>
--- App/Views/nav.php
+++ App/Views/nav.php
@@ -1,4 +1,4 @@
 <nav>
 	<a href="/">Home</a>
-	<?php echo ($user_object) ? '| <a href="/profile/show">Profile</a>' : ''; ?>
+	<?php echo ($user_object) ? '| <a href="/profile/show">Profile</a> | <a href="/logout">Logout</a>' : ''; ?>
 </nav>
